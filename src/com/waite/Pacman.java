423411d051b031c4e477897e290995fc9462ed73
rails new simpleSite <ruby>
2023-04-25 14:04:54 +0300

    Initial commit

---
+++ src/com/waite/Pacman.java
@@ -0,0 +1,31 @@
+package com.waite;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Pacman extends JFrame {
+
+    public Pacman() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        add(new Board());
+
+        setTitle("PACMAN");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(380, 420);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+
+            var ex = new Pacman();
+            ex.setVisible(true);
+        });
+    }
+}
